47e4f896f0a22b976ca73a230f23fe6cfd5252a9
jiunlee19 <50683915+jiunlee19@users.noreply.github.com>
2020-12-31 15:44:02 +0900

    구구단
    2020.12.30 구구단 해체 송별코딩

---
+++ gugudan.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int gugudan(int firstNum, int secondNum) {
+	int result = 0;
+	result = firstNum * secondNum;
+	return result;
+}
+
+int main(void) {
+	int result;
+
+	for (int firstNum = 1; firstNum <= 9; firstNum++) {
+		printf("%d´Ü-------\n", firstNum);
+		for (int secondNum = 1; secondNum <= 9; secondNum++) {
+
+			printf("%d x %d = %d\n", firstNum, secondNum, gugudan(firstNum, secondNum));
+		}
+	}
+}
