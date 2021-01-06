82fd939e5f0641d8666b24ef127fbec820470798
jiunlee19 <50683915+jiunlee19@users.noreply.github.com>
2021-01-06 17:02:05 +0900

    Add files via upload

---
+++ 한수구하기_1065.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#define underHundred 99
+
+int howmany(int N) {
+	int count = underHundred;
+	int a,b,c;
+
+	for (int i = 100; i <= N; i++) {
+		a = i / 100;
+		b = (i % 100) / 10;
+		c = i % 10;
+
+		if (c-b == b-a) count++;
+	}
+
+	return count;
+}
+
+int main(void) {
+	int N;
+	int hansu;
+
+	scanf_s("%d", &N);
+	if (N<100) hansu = N;
+	else hansu = howmany(N);
+	printf("%d", hansu);
+}
